e8cb86e3a0a7e9b4b7a46a94e2e9942388c4b62b
Anna Mendoza <annamendoza.curve@gmail.com>
2017-09-28 00:39:09 -0700

    adding the two different ways to construct the navbar

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 export default function NavigationBar(props) {
+	// the MAPPING METHOD APPROACH
 	const all_links = props.links.map((link, index) => (
 		<li className="topNav" key={index}>
 			<a href={link.href} target="_blank">
 				{link.text}
 			</a>
 		</li>
-	));	
+	));
+
+	// the FOR LOOP APPROACH
+	const footer_links = [];
+	const numbers = footer_links.index;
+  for (let i=0; i < props.links.length; i++) {
+  	footer_links.push(
+  		<li className="topNav" key={i.toString()}>
+  			<a href={props.links[i].href} target="_blank">
+  				{props.links[i].text}
+  			</a>
+  		</li>
+  	)
+  };
 
 	return (
 		<div className="navbar_styling">
@@ -16,7 +30,7 @@ export default function NavigationBar(props) {
 			</div>
 	    <nav className="all_links">
 	    	<ul className="right_side">
-	    		{all_links}
+	    		{footer_links}
 	    	</ul>
 	    </nav>
 	  </div>
